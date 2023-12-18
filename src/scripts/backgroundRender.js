b032475b4fa9a8ba1403218ae7ffc8a8be528811
Jacob Tabor <jtabor214@gmail.com>
2023-12-17 23:04:39 -0500

    Created BackgroundRender and FeatRender classes; defined constructors and populated the DOM; formatted information drawn from API using CSS

---
+++ src/scripts/backgroundRender.js
@@ -0,0 +1,47 @@
+import { marked } from "marked";
+
+class BackgroundRender{
+
+  constructor(data) {
+    this.data = data;
+	  this.populateClassDOM(data);
+  }
+
+	populateClassDOM(data){
+		const backgroundList = document.getElementById('visualContainer');
+		const list = document.createElement('ul');
+		list.classList.add('backgroundTitles');
+
+		data.results.forEach(background => {
+			const backgroundEle = document.createElement('button');
+			backgroundEle.classList.add('title');
+			backgroundEle.textContent = background.name;
+			backgroundEle.addEventListener('click', ev => {
+				renderBackgroundDesc(ev, background);
+			});
+
+			list.appendChild(backgroundEle);
+		});
+
+		backgroundList.innerHTML = '';
+		backgroundList.appendChild(list);
+	};
+};
+
+const renderBackgroundDesc = (ev, background) => {
+	const backgroundInfo = document.getElementById('dataContainer');
+	const list = document.createElement('ul');
+	const targetInfo = [background.desc, background.skill_proficiencies, background.equipment, background.suggested_characteristics];
+
+	targetInfo.forEach(info => {
+		const backgroundStats = document.createElement('p');
+		backgroundStats.innerHTML = marked.parse(info);
+		list.appendChild(backgroundStats);
+	});
+
+	backgroundInfo.innerHTML = '';
+	backgroundInfo.appendChild(list);
+
+};
+
+export default BackgroundRender;
