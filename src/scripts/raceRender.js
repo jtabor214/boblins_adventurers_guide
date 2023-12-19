11fe11d1ae42a29145201a8d7c5c7427f490d36d
Jacob Tabor <jtabor214@gmail.com>
2023-12-18 23:16:34 -0500

    Created characters directory in assests folder and included character asset images to the folder

--- src/scripts/raceRender.js
+++ src/scripts/raceRender.js
@@ -1,10 +1,24 @@
 import { marked } from "marked";
 
+// import dragonborn from '../../assets/characters/dragonborn.png';
+
 class RaceRender{
   
   constructor(data) {
 		  this.data = data;
 		  this.populateRaceDOM(data);
+      // this.image = new Image();
+      // this.images = ['../../assets/characters/dragonborn.png',
+      //  '../../assets/characters/dwarf.png',
+      //  '../../assets/characters/elf.png',
+      //  '../../assets/characters/halfling.png', 
+      //  '../../assets/characters/human.png',
+      //  '../../assets/characters/dragonborn.png', 
+      //  '../../assets/characters/gnome.png', 
+      //  '../../assets/characters/halfelf.png', 
+      //  '../../assets/characters/halforc.png', 
+      //  '../../assets/characters/tiefling.png'];
+
   }
 
   populateRaceDOM(data){
@@ -13,21 +27,21 @@ class RaceRender{
 		list.classList.add('raceNames');
 
     data.results.forEach(race => {
+      console.log(this);
       const raceEle = document.createElement('button');
       raceEle.classList.add('name');
       raceEle.textContent = race.name;
+
 			raceEle.addEventListener('click', ev => {
 				renderRaceDesc(ev, race);
 			});
       list.appendChild(raceEle);
-      // const icon = document.createElement('IMG');
-      // const propImg = 'bungie.net' + displayProperties.icon;
-      // icon.src = propImg;
-      // ul.appendChild(icon);
+    
     });
 
 		raceList.innerHTML = '';
     raceList.appendChild(list);
+    
   };
 }
 
@@ -36,15 +50,13 @@ const renderRaceDesc =  (ev, race) => {
   const list = document.createElement('ul');
   const targetInfo = [race.desc, race.size, race.traits, race.vision];
 
-  // unecessary
+
   targetInfo.forEach(info => {
     const raceStats = document.createElement('p');
     raceStats.innerHTML = marked.parse(info);
-
-    // markdown translation goes here
     list.appendChild(raceStats);
   });
-  console.log(raceInfo);
+  
 
   raceInfo.innerHTML = '';
   raceInfo.appendChild(list);
