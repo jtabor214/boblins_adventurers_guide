e4fb0fd77d9c9d83b4c082ba0ef7c4db6b20947f
Jake Tabor <jtabor214@gmail.com>
2025-02-22 17:53:07 -0500

    Fill out all default rendering fields - map through display fields and filter null/undefined fields

--- src/scripts/baseRenderer.js
+++ src/scripts/baseRenderer.js
@@ -2,6 +2,63 @@ import { marked } from "marked";
 
 class BaseRenderer {
   constructor(data, config) {
-    
+    this.data = data;
+
+    this.config = {
+      containerClass: '',
+      imagePrefix: '',
+      imageType: '',
+      displayFields: [],
+      gridColumns: 1,
+      ...config
+    };
+
+    this.populateDOM(data)
+  }
+
+  populateDOM(data) {
+    const container = document.getElementById('visualContainer');
+    const list = document.createElement('ul');
+    list.classList.add(this.config.containerClass)
+
+    data.results.forEach(item => {
+      const itemContainer = document.createElement('div');
+      const button = document.createElement('button');
+      button.classList.add('title');
+      button.textContent = item.name;
+
+      if (this.config.imagePrefix) {
+        const image = document.createElement('img');
+        image.classList.add (`${this.config.imageType}-image`);
+        const imageName = item.name.replace(/-/g, '').toLowerCase();
+        image.src = `./assets/${this.config.imagePrefix}/${imageName}.png`;
+        itemContainer.appendChild(image);
+      }
+
+      itemContainer.appendChild(button);
+      button.addEventListener('click', () => this.renderDescription(item));
+      list.appendChild(itemContainer);
+    });
+
+    container.innerHTML = '';
+    container.appendChild(list);
+  }
+
+  renderDescription(item) {
+    const container = document.getElementById('dataContainer');
+    const list = document.createElement('ul');
+
+    const info = this.config.displayFields.map(field => item[field]).filter(Boolean);
+
+    info.forEach(content => {
+      const element = document.createElement('p');
+      element.innerHTML = marked.parse(content.toString());
+      list.appendChild(element);
+    });
+
+    container.innerHTML = '';
+    container.appendChild(list);
   }
 }
+
+export default BaseRenderer;
